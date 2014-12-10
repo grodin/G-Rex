da32997d456267e01878f128c53dda56051d2ae4
Joseph Cooper <cooper.joseph@gmail.com>
2014-12-10 16:36:19 +0100

    Added check that directory argument to GRexCorePersister is actually a directory

--- grex-core/src/main/java/au/com/gridstone/grex/GRexCorePersister.java
+++ grex-core/src/main/java/au/com/gridstone/grex/GRexCorePersister.java
@@ -6,6 +6,7 @@ import java.io.File;
 
 import au.com.gridstone.grex.converter.Converter;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class GRexCorePersister extends BaseGRexPersister {
@@ -13,14 +14,20 @@ public class GRexCorePersister extends BaseGRexPersister {
     /**
      * Create a new instance using a provided {@link Converter}.
      *
-     * @param converter Converter used to serialize/deserialize objects.
+     * @param converter Converter used to serialize/deserialize objects,
+     *                  not null
+     * @param directory Directory to write/read files,
+     *                  not null. {@link File#isDirectory()} must return true
+     *                  on this parameter
      */
-    public GRexCorePersister(final Converter converter) {
+    public GRexCorePersister(@NotNull final Converter converter,
+                             @NotNull final File directory) {
         super(checkNotNull(converter), new FileFactory() {
             @NotNull
             @Override
             public File getFile(final String key) {
-                return null;
+                checkArgument(checkNotNull(directory).isDirectory());
+                return new File(directory,key);
             }
         });
     }
